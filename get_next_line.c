7370a56cfed6e69aab55a7beb482793bd2972f21
julesfieux <julesfieux@yahoo.fr>
2021-01-11 16:53:25 +0100

    modif 2

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,18 @@
 /*   By: jfieux <jfieux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 10:44:24 by jfieux            #+#    #+#             */
-/*   Updated: 2021/01/11 10:46:08 by jfieux           ###   ########.fr       */
+/*   Updated: 2021/01/11 16:53:02 by jfieux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 int get_next_line(int fd, char **line)
 {
-    
+    int ret;
+    char *buf[BUFFER_SIZE];
+
+    if (fd < 0 || !line || BUFFER_SIZE < 1 || read(fd, NULL, 0) == -1)
+		return (-1);
+	
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,16 @@
 /*   By: jfieux <jfieux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 10:42:16 by jfieux            #+#    #+#             */
-/*   Updated: 2021/01/11 11:33:11 by jfieux           ###   ########.fr       */
+/*   Updated: 2021/01/11 16:21:00 by jfieux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <unistd.h>
+
+int BUFFER_SIZE;
 int get_next_line(int fd, char **line);
 
 #endif
