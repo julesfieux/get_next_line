c2ab709df31565d7a7b929ffe1bfc9daf6377add
julesfieux <julesfieux@yahoo.fr>
2021-01-12 10:56:23 +0100

    modif 3

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,37 @@
 /*   By: jfieux <jfieux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 10:44:24 by jfieux            #+#    #+#             */
-/*   Updated: 2021/01/11 16:53:02 by jfieux           ###   ########.fr       */
+/*   Updated: 2021/01/12 10:55:30 by jfieux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char *ft_new_line(char *save)
+{
+	
+}
+
 int get_next_line(int fd, char **line)
 {
-    int ret;
-    char *buf[BUFFER_SIZE];
+    int			ret;
+    char		*buf;
+	static char	*save;
 
-    if (fd < 0 || !line || BUFFER_SIZE < 1 || read(fd, NULL, 0) == -1)
+    if (fd < 0 || !line || BUFFER_SIZE < 1)
 		return (-1);
-	
+	if (!(buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	while (!ft_found_return(save) && ret != 0)
+	{
+		if ((ret = read(fd, buf, BUFFER_SIZE)) == -1)
+		{
+			free(buf);
+			return (-1);
+		}
+		buf[ret] = '\0';
+		save = ft_strjoin(save, buf);
+	}
+	free (buf);
+	*line = ft_new_line(save);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,70 @@
 /*   By: jfieux <jfieux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 10:44:51 by jfieux            #+#    #+#             */
-/*   Updated: 2021/01/11 10:44:52 by jfieux           ###   ########.fr       */
+/*   Updated: 2021/01/12 10:42:38 by jfieux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+int		ft_strlen(const char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*join_init(char *res, char const *s1, char const *s2, int i)
+{
+	int j;
+
+	j = 0;
+	while (s1[i])
+	{
+		res[j] = s1[i];
+		i++;
+		j++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		res[j] = s2[i];
+		i++;
+		j++;
+	}
+	res[j] = '\0';
+	return (res);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		len;
+	char	*res;
+
+	if (!s1 || !s2)
+		return (0);
+	len = (ft_strlen(s1) + ft_strlen(s2));
+	if (!(res = malloc(sizeof(char) * (len + 1))))
+		return (0);
+	join_init(res, s1, s2, 0);
+	return (res);
+}
+
+int		ft_found_return(char *str)
+{
+	int i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
